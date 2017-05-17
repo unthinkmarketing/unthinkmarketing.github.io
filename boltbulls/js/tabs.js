8edc5c9701d84e4d0bb87b8513fa9bc5242986f4
gencfw <gen.cfw@gmail.com>
2017-05-16 20:01:05 -0600

    Added popup, still looks simple

--- boltbulls/js/tabs.js
+++ boltbulls/js/tabs.js
@@ -1,18 +1,38 @@
-$(document).ready(function(){
-	
-	$('.tab-link').click(function(){
-		var tab_id = $(this).attr('data-tab');
+$(document).ready(function () {
+    // Get the modal
+    var modal = document.getElementById('myModal');
 
-		$('.tab-link').removeClass('current');
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    $('.tab-link').click(function () {
+        var tab_id = $(this).attr('data-tab');
+
+        $('.tab-link').removeClass('current');
         $('.tab-link').removeClass('no-border');
-		$('.tab-content').removeClass('current');
+        $('.tab-content').removeClass('current');
         $('.triangle').removeClass('current');
-        
 
-		$(this).addClass('current');
+
+        $(this).addClass('current');
         $(this).addClass('no-border');
-		$("#tab-"+tab_id).addClass('current');
-        $("#tri-"+tab_id).addClass('current');
-	})
+        $("#tab-" + tab_id).addClass('current');
+        $("#tri-" + tab_id).addClass('current');
+    });
 
-})
+
+    $('.results-tab-li').click(function () {
+        modal.style.display = "block";
+    });
+
+    $('.close').click(function () {
+        modal.style.display = "none";
+    });
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
