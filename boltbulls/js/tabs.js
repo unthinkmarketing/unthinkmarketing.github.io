eadf2c664088f13d2233251e97e30088bb48129d
gencfw <gen.cfw@gmail.com>
2017-05-17 21:13:14 -0600

    Results popup is now dynamic

--- boltbulls/js/tabs.js
+++ boltbulls/js/tabs.js
@@ -5,6 +5,12 @@ $(document).ready(function () {
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
+    var modalHead = $('#modal-h2');
+    var modalBody = $('#modal-p');
+    var modalFooter = $('#modal-a');
+
+    clearModal();
+
     $('.tab-link').click(function () {
         var tab_id = $(this).attr('data-tab');
 
@@ -21,18 +27,44 @@ $(document).ready(function () {
     });
 
 
-    $('.results-tab-li').click(function () {
-        modal.style.display = "block";
+    $('.results-warning').click(function () {
+        var warning = $(this).attr('data-tab');
+
+        if (warning === "min") {
+            modalFooter.attr("href", "http://betabulls.com/");
+            setModal("You can optimize by doing this:", "Minifying css and js files in order to decrease total page size", "Don't believe us? Click here to see it in action.");
+        } else if (warning === "compat") {
+            modalFooter.attr("href", "http://www.google.com/");
+            setModal("Browser compatability", "Some of your code is not widely compatible, this can be fixed by using prefixes or APIs.", "Click here to see a list of the most widely used browers.");
+        } else if (warning === "testing-war") {
+            modalFooter.attr("href", "http://betabulls.com/");
+            setModal("Problem title", "Error description, this won't appear when we don't have anything to say", "CTA or similar item");
+        }
     });
 
     $('.close').click(function () {
+        clearModal();
         modal.style.display = "none";
     });
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
         if (event.target == modal) {
+            clearModal();
             modal.style.display = "none";
         }
+    }
+
+    function clearModal() {
+        modalHead.text("");
+        modalBody.text("");
+        modalFooter.text("");
+    }
+
+    function setModal(headText, bodyText, footerText) {
+        modalHead.text(headText);
+        modalBody.text(bodyText);
+        modalFooter.text(footerText);
+        modal.style.display = "block";
     }
 });
